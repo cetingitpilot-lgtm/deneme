1bd0a5e076e4134a6041401c8476022fbf398a26
cetingitpilot-lgtm <cetin.githubcopilot@gmail.com>
2026-01-13 11:58:21 +0300

    Update ema30.py

--- indicators/ema30.py
+++ indicators/ema30.py
@@ -4,5 +4,5 @@ import pandas_ta as ta
 BILGI = {"ad": "EMA 30", "tip": "overlay"}
 
 def çiz(fig, df, row):
-    df['ema7'] = ta.ema(df['Close'], length=30)
+    df['ema30'] = ta.ema(df['Close'], length=30)
     fig.add_trace(go.Scatter(x=df.index, y=df['ema30'], name='EMA 30'), row=row, col=1)
