ac8f8be2f91d113d9c06a1d5beaf8b9937886041
cetingitpilot-lgtm <cetin.githubcopilot@gmail.com>
2026-01-13 11:56:22 +0300

    Update volume.py

--- indicators/volume.py
+++ indicators/volume.py
@@ -1,10 +1,7 @@
 import plotly.graph_objects as go
 
-NAME = "Hacim"
-TYPE = "oscillator"
+BILGI = {"ad": "Hacim", "tip": "oscillator"}
 
-def ciz(fig, df, x_axis, row):
-    # Hacim barlarını renkli yapalım (Yükseliş yeşil, düşüş kırmızı)
-    colors = ['green' if df['Close'][i] >= df['Open'][i] else 'red' for i in range(len(df))]
-    fig.add_trace(go.Bar(x=x_axis, y=df['Volume'], name=NAME, marker_color=colors, opacity=0.5), row=row, col=1)
-    return fig
+def çiz(fig, df, row):
+    renkler = ['#26a69a' if c >= o else '#ef5350' for c, o in zip(df['Close'], df['Open'])]
+    fig.add_trace(go.Bar(x=df.index, y=df['Volume'], name='Hacim', marker_color=renkler), row=row, col=1)
