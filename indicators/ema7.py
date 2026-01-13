9bf9b1f51d51bdfd01ab692d95655ec88122fa6d
cetingitpilot-lgtm <cetin.githubcopilot@gmail.com>
2026-01-13 11:58:46 +0300

    Update ema7.py

--- indicators/ema7.py
+++ indicators/ema7.py
@@ -1,11 +1,8 @@
-import pandas_ta as ta
 import plotly.graph_objects as go
+import pandas_ta as ta
 
-NAME = "EMA 7"
-TYPE = "overlay"
+BILGI = {"ad": "EMA 7", "tip": "overlay"}
 
-def ciz(fig, df, x_axis, row):
-    df['EMA7'] = ta.ema(df['Close'], length=7)
-    fig.add_trace(go.Scatter(x=x_axis, y=df['EMA7'], name=NAME, 
-                             line=dict(color='yellow', width=1.5)), row=1, col=1)
-    return fig
+def çiz(fig, df, row):
+    df['ema7'] = ta.ema(df['Close'], length=7)
+    fig.add_trace(go.Scatter(x=df.index, y=df['ema7'], name='EMA 7'), row=row, col=1)
