7e4195e69d47bf323679386428445959bf3d250b
cetingitpilot-lgtm <cetin.githubcopilot@gmail.com>
2026-01-13 11:34:16 +0300

    Update __init__.py

--- indicators/__init__.py
+++ indicators/__init__.py
@@ -1,18 +1,11 @@
 import os
 import importlib
-import streamlit as st
 
-package_dir = os.path.dirname(__file__)
-files = [f[:-3] for f in os.listdir(package_dir) if f.endswith('.py') and f != '__init__.py']
+MODULES = {}
 
-MODULLER = {}
-for f in files:
-    try:
-        module = importlib.import_module(f'indicators.{f}')
-        if hasattr(module, 'NAME'):
-            MODULLER[module.NAME] = module
-    except IndentationError as e:
-        st.error(f"DİKKAT! '{f}.py' dosyasında boşluk hatası var. Lütfen bu dosyayı kontrol et.")
-        # Hatayı detaylı görmek istersen: st.exception(e)
-    except Exception as e:
-        print(f"Hata {f}: {e}")
+current_dir = os.path.dirname(__file__)
+
+for file in os.listdir(current_dir):
+    if file.endswith(".py") and file not in ["__init__.py"]:
+        name = file.replace(".py", "")
+        MODULES[name] = importlib.import_module(f"indicators.{name}")
